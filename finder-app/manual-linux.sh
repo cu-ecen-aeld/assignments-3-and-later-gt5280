94e701f9fe8f80f9002ec2eb0b6215f94a29a137
nero2121 <nero2121@colorado.edu>
2022-02-26 16:49:41 -0700

    3_2tx

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -20,11 +20,12 @@ else
 	OUTDIR=$1
 	echo "Using passed directory ${OUTDIR} for output"
 fi
-
+if [ ! -d "${OUTDIR}" ]
 mkdir -p ${OUTDIR}
-
+else
 cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/linux-stable" ]; then
+if [ ! -d "${OUTDIR}/linux-stable" ]; 
+then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
@@ -34,6 +35,12 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
+	make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+	make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+	make ARCH=arm64 -j4 CROSS_COMPILE=aarch64-none-linux-gnu- all
+	make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+	make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs	
+	
     # TODO: Add your kernel build steps here
 fi 
 
@@ -47,6 +54,13 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
+mkdir rootfs
+cd rootfs
+mkdir bin dev etc home lib proc sbin sys tmp usr va var
+mkdir bin usr/bin usr/sbin usr/lib
+mkdir -p var/log
+sudo chown -R root:root *
+
 # TODO: Create necessary base directories
 
 cd "$OUTDIR"
@@ -55,10 +69,17 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
+	
+	make distclean
+	make defconfig
+	
     # TODO:  Configure busybox
 else
     cd busybox
-fi
+
+sudo make ARCH=arm64 CROSS_COMPILE=/usr/local/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu- install CONFIG_PREFIX=/home/nero2121/myproject/assignment-1-gt5280/outdir/rootfs
+
+cd ${OUTDIR}/rootfs
 
 # TODO: Make and insatll busybox
 
@@ -66,15 +87,42 @@ echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
+sudo cp -a /usr/local/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 lib
+sudo cp -a /usr/local/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/ld-2.31.so lib
+sudo cp -a /usr/local/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 lib
+sudo cp -a /usr/local/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv-2.31.so lib
+sudo cp -a /usr/local/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 lib
+sudo cp -a /usr/local/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc-2.31.so lib
+sudo cp -a /usr/local/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 lib
+sudo cp -a /usr/local/arm-cross-compiler/install/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm-2.31.so lib
+
+
 # TODO: Add library dependencies to rootfs
 
+#sudo mknod -m 666 dev/null c 1 3
+#sudo mknod -m 600 dev/console c 5 1
+
 # TODO: Make device nodes
+
+#cd ${OUTDIR}/linux-stable
+
+#sudo make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- INSTALL_MOD_PATH=/home/nero2121/myproject/assignment-1-gt5280/outdir/linux-stable modules_install
 
 # TODO: Clean and build the writer utility
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
 
+ 
 # TODO: Chown the root directory
 
+#cd ${OUTDIR}/rootfs
+#sudo chown -R root:root *
+#find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
+#cd ..
+#zip initramfs.cpio 
+
+
 # TODO: Create initramfs.cpio.gz
+#QEMU_AUDIO_DRV=none qemu-system-arm -m 256 -nographic -M versatilepb -kernel zImage -append "console=ttyAMA0 rdinit=bin/sh" -dtb versatile-pb.dtb-initrd initramfs.cpio.gz
+
